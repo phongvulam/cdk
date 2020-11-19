6c97c823921f8bdae48879f7dcf592e6b8a90f15
phongvulam <6957692+phongvulam@users.noreply.github.com>
2020-11-19 13:49:22 +0700

    init

--- ecs-fargate/lib/ecs-fargate-stack.ts
+++ ecs-fargate/lib/ecs-fargate-stack.ts
@@ -1,12 +1,5 @@
 import * as cdk from '@aws-cdk/core';
-import * as ec2 from "@aws-cdk/aws-ec2";
-import * as ecs from "@aws-cdk/aws-ecs";
-import * as ecs_patterns from "@aws-cdk/aws-ecs-patterns";
 
-
-/**
- * To deploy a SpringBoot application using CDK.
- */
 export class EcsFargateStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
